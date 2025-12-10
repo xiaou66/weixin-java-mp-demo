6f1f7df6ee1283f48b23d74cb1e5626245683a71
xiaou <2630316030@qq.com>
2025-12-10 20:47:35 +0800

    INIT

---
+++ src/main/java/cn/xiaou/weixin/mp/demo/WechatMpApplication.java
@@ -0,0 +1,16 @@
+package cn.xiaou.weixin.mp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author xiaou
+ * @date 2025/12/10
+ */
+@SpringBootApplication
+public class WechatMpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WechatMpApplication.class);
+    }
+}
